c8844335778416a86f0d20bf08dee7cf12edc0f6
Kop7er <58952825+Kop7er@users.noreply.github.com>
2021-07-03 22:03:49 +0100

    Progress On The Note Command

--- src/commands/note.js
+++ src/commands/note.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { Prefix, EmbedColor } = require('../config/config.json').Bot;
+const { AnyoneCanAddNotes } = require('../config/config.json').Tickets;
 
 module.exports = {
 
@@ -9,6 +10,24 @@ module.exports = {
 
     async execute(bot, message, args, con) 
     {
+        try 
+        {
+            let TicketNumber = args[1];
 
+            if (!TicketNumber || isNaN(TicketNumber)) return message.channel.send("Please provide a Ticket Number!");
+
+            const [ TicketData ] = await con.execute(`SELECT * FROM Tickets WHERE TicketNumber = '${TicketNumber}';`);
+
+            if (TicketData.length < 1) return message.channel.send("Invalid Ticket Number!");
+
+            if (!(AnyoneCanAddNotes && TicketData[0].HandledBy === message.author.id)) return message.channel.send("You don't have permission to a note to this ticket!");
+        } 
+        
+        catch (error) 
+        {
+            console.error(error);
+    
+            message.channel.send("There was an error adding a note to this ticket!");        
+        }
     }
 };
