24a61f5f0b56104f95395bc70c196e0059507624
Kop7er <58952825+Kop7er@users.noreply.github.com>
2021-07-03 22:03:35 +0100

    DB File Update

--- src/database/db.sql
+++ src/database/db.sql
@@ -10,3 +10,13 @@ CREATE TABLE IF NOT EXISTS Tickets
     ClosedOn VARCHAR(100) NULL,
     PRIMARY KEY (TicketNumber)
 );
+
+CREATE TABLE IF NOT EXISTS TicketsNotes
+(
+    TicketNumber INT(100) NOT NULL,
+    Note TEXT NOT NULL,
+    NotedBy VARCHAR(100) NOT NULL,
+    NotedOn VARCHAR(100) NOT NULL,
+    PRIMARY KEY (TicketNumber),
+    FOREIGN KEY (TicketNumber) REFERENCES Tickets(TicketNumber)
+);
